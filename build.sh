07d50f331b0cc3cb01418f938e66935df639e913
Elena Neroslavskaya <lenisha@gmail.com>
2018-04-29 02:14:51 -0400

    add labels

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t vsts-agent-infrastructure --build-arg VCS_REF=`git rev-parse --short HEAD` .
+docker build -t vsts-agent-infrastructure --build-arg VCS_REF="git rev-parse --short HEAD" .
+docker tag vsts-agent-infrastructure lenisha/vsts-agent-infrastructure 
+docker push lenisha/vsts-agent-infrastructure 
