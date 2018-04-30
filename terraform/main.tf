9c5db5b7d79b621cd890684b22d52b7a69c7de3a
Elena Neroslavskaya <lenisha@gmail.com>
2018-04-29 22:21:50 -0400

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+resource "azurerm_resource_group" "aci-rg" {
+  name     = "aci-vsts"
+  location = "westus2"
+}
+
+resource "azurerm_storage_account" "aci-sa" {
+  name                = "acivststorageacctount"
+  resource_group_name = "${azurerm_resource_group.aci-rg.name}"
+  location            = "${azurerm_resource_group.aci-rg.location}"
+  account_tier        = "Standard"
+
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "aci-share" {
+  name                 = "aci-vsts-share"
+  resource_group_name  = "${azurerm_resource_group.aci-rg.name}"
+  storage_account_name = "${azurerm_storage_account.aci-sa.name}"
+
+  quota = 50
+}
+
+resource "azurerm_container_group" "aci-vsts" {
+  name                = "aci-agent"
+  location            = "${azurerm_resource_group.aci-rg.location}"
+  resource_group_name = "${azurerm_resource_group.aci-rg.name}"
+  ip_address_type     = "public"
+  os_type             = "linux"
+
+  container {
+    name   = "vsts-agent"
+    image  = "lenisha/vsts-agent-infrastructure"
+    cpu    = "0.5"
+    memory = "1.5"
+    port   = "80"
+
+    environment_variables {
+      "VSTS_ACCOUNT" = "${var.vsts-account}"
+      "VSTS_TOKEN"   = "${var.vsts-token}"
+      "VSTS_AGENT"   = "${var.vsts-agent}"
+      "VSTS_POOL"    = "${var.vsts-pool}"
+    }
+
+    volume {
+      name       = "logs"
+      mount_path = "/aci/logs"
+      read_only  = false
+      share_name = "${azurerm_storage_share.aci-share.name}"
+
+      storage_account_name = "${azurerm_storage_account.aci-sa.name}"
+      storage_account_key  = "${azurerm_storage_account.aci-sa.primary_access_key}"
+    }
+  }
+
+  tags {
+    environment = "testing"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "vsts-account" {
+  default = "eneros"
+}
+
+variable "vsts-token" {
+  default = ""
+}
+
+variable "vsts-agent" {
+  default = "ACI-Agent"
+}
+
+variable "vsts-pool" {
+  default = "ACI-Pool"
+}
