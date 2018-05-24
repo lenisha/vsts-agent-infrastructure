4915dc3ac000f9241f480f6979594f22de3768d2
Elena Neroslavskaya <lenisha@gmail.com>
2018-05-24 11:27:26 -0400

    shortername

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ resource "random_id" "randomId" {
 }
 
 resource "azurerm_storage_account" "aci-sa" {
-  name                = "aciagentstorage${random_id.randomId.hex}"
+  name                = "acisa${random_id.randomId.hex}"
   resource_group_name = "${azurerm_resource_group.aci-rg.name}"
   location            = "${azurerm_resource_group.aci-rg.location}"
   account_tier        = "Standard"
